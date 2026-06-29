72074cba317664c2e4668a21151d832d373e481c
kreczko <lkreczko@googlemail.com>
2026-06-29 09:12:41 +0100

    fix: remove stray "\"

--- RuneHelper/ocr/NameNormalizer.h
+++ RuneHelper/ocr/NameNormalizer.h
@@ -4,4 +4,4 @@
 
 std::string NormalizeName(std::string s);
 double Similarity(const std::string& a, const std::string& b);
-std::optional<std::string> FindBestItemMatch(const std::string& ocrText, const std::vector<std::string>& dbNames);\
+std::optional<std::string> FindBestItemMatch(const std::string& ocrText, const std::vector<std::string>& dbNames);
